3b3434b93b67cc120d969fb05f7da66673ceb25e
909-SAM-909 <alvarezrodriguez272@gmail.com>
2024-12-18 01:06:32 +0100

    ciao

---
+++ ciao.java
@@ -0,0 +1,28 @@
+public class Esempio {
+
+	public static void main(String[] args) {
+			System.out.println("Ciao Mamma!");
+
+	int a;
+	a = 50;
+	a = 30;
+	int b =50;
+	System.out.println(a);
+	// assegno a=30
+	a = 30;
+		//dichiaro int c, d, h=10, 	
+	int c, d, h=10, f;
+	byte m=100; 
+	short g=200;
+	long p=5000;
+	boolean z=true;
+	z=false;
+	System.out.println(z);
+	char temp='f';
+	System.out.println("Il valore è"+ temp);
+	
+	
+	
+	
+	}
+}
